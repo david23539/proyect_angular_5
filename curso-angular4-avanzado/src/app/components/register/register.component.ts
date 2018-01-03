a76f65324d937910db48373176de7247f6d5c5ad
david23539 <david23539@gmail.com>
2018-01-03 12:01:24 +0100

    nuevos cambios

---
+++ curso-angular4-avanzado/src/app/components/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from "@angular/router";
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  public title: String;
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) {
+    this.title = "Registro";
+  }
+
+  ngOnInit() {
+    console.log("Register. component cargado");
+  }
+
+}
